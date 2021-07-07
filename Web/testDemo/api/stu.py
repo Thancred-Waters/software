1a3e780ac1139478f5d00be947c66acb251e549e
xuanranxiaoshi_gitk <2479966011@qq.com>
2021-07-07 20:57:11 +0800

    进行了简单的格式化； 提交的一个尝试；

--- Web/testDemo/api/stu.py
+++ Web/testDemo/api/stu.py
@@ -1,52 +1,56 @@
-from fastapi import APIRouter,Query
+from fastapi import APIRouter, Query
 
 from pydantic import BaseModel
+
 # 构建api路由
 router = APIRouter()
 
 from testDemo.service import userService
 
-class Res(BaseModel) :
-    code:str = ""
-    msg:str = ""
-    data:dict = {}
-    
-@router.get("/show",tags=["users"],response_model=Res)
+
+class Res(BaseModel):
+    code: str = ""
+    msg: str = ""
+    data: dict = {}
+
+
+@router.get("/show", tags=["users"], response_model=Res)
 async def show():
-    tmp=userService.showUser()
-    msg=tmp[0]; con=tmp[1]
-    res=dict()
-    res.update({"code":"1"})
-    res.update({"msg":msg})
-    res.update({"data":con})
-    return res
-
-@router.get("/add", tags=["users"],response_model=Res)
-async def add(id:str,name:str,age:int = Query(...,ge=0,le=150)):
-    msg=userService.addUser(id,name,age)
-    res=dict()
-    res.update({"code":"2"})
-    res.update({"msg":msg})
-    res.update({"data":{}})
-    return res
-
-@router.get("/delete",tags=["users"],response_model=Res)
-async def delete(id:str):
-    msg=userService.deleteUser(id)
-    res=dict()
-    res.update({"code":"3"})
-    res.update({"msg":msg})
-    res.update({"data":{}})
-    return res
-
-@router.get("/edit",tags=["users"],response_model=Res)
-async def edit(id:str,name:str,age:int = Query(...,ge=0,le=150)):
-    msg=userService.editUser(id,name,age)
-    res=dict()
-    res.update({"code":"4"})
-    res.update({"msg":msg})
-    res.update({"data":{}})
+    tmp = userService.showUser()
+    msg = tmp[0];
+    con = tmp[1]
+    res = dict()
+    res.update({"code": "1"})
+    res.update({"msg": msg})
+    res.update({"data": con})
     return res
 
 
-    
+@router.get("/add", tags=["users"], response_model=Res)
+async def add(id: str, name: str, age: int = Query(..., ge=0, le=150)):
+    msg = userService.addUser(id, name, age)
+    res = dict()
+    res.update({"code": "2"})
+    res.update({"msg": msg})
+    res.update({"data": {}})
+    return res
+
+
+@router.get("/delete", tags=["users"], response_model=Res)
+async def delete(id: str):
+    msg = userService.deleteUser(id)
+    res = dict()
+    res.update({"code": "3"})
+    res.update({"msg": msg})
+    res.update({"data": {}})
+    return res
+
+
+@router.get("/edit", tags=["users"], response_model=Res)
+async def edit(id: str, name: str, age: int = Query(..., ge=0, le=150)):
+    msg = userService.editUser(id, name, age)
+    res = dict()
+    res.update({"code": "4"})
+    res.update({"msg": msg})
+    res.update({"data": {}})
+    return res
